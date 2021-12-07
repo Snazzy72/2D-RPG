ec7354daa7b0b612a6298b43ba236f4b4c232bb8
Snazzy72 <69359713+Snazzy72@users.noreply.github.com>
2021-12-07 15:39:02 +0200

    Add files via upload

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset;
+   
+	private void FixedUpdate()
+	{
+        HandleCameraMovement();
+    }
+
+	private void HandleCameraMovement()
+	{
+        transform.position = new Vector3(player.transform.position.x + offset.x,
+            player.transform.position.y + offset.y, player.transform.position.z + offset.z);
+	}
+}
